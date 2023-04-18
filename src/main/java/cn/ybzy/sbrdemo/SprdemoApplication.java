2572a9d00b5d4b27bae91d82031a3b945e3effa6
xiongshaowen <65179334@qq.com>
2023-04-18 09:26:20 +0800

    init

---
+++ src/main/java/cn/ybzy/sbrdemo/SprdemoApplication.java
@@ -0,0 +1,14 @@
+package cn.ybzy.sbrdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SprdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprdemoApplication.class, args);
+    }
+
+}
